462e6924704dc1685469212e156e35fedee8eb8b
jayyymorales <jrmorales@ymail.com>
2017-10-25 21:41:31 -0700

    final

---
+++ app/data/friends.js
@@ -0,0 +1,101 @@
+var friends = [
+  {
+    name: "Chip",
+    photo: "https://static.pexels.com/photos/160846/french-bulldog-summer-smile-joy-160846.jpeg",
+    scores: [
+      "5",
+      "1",
+      "4",
+      "4",
+      "5",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1"
+    ]
+  },
+  {
+    name: "Benny",
+    photo: "https://static.pexels.com/photos/159541/wildlife-photography-pet-photography-dog-animal-159541.jpeg",
+    scores: [
+      "4",
+      "2",
+      "5",
+      "1",
+      "3",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2"
+    ]
+  },
+  {
+    name: "Max",
+    photo: "https://static.pexels.com/photos/97082/weimaraner-puppy-dog-snout-97082.jpeg",
+    scores: [
+      "5",
+      "2",
+      "2",
+      "2",
+      "4",
+      "1",
+      "3",
+      "2",
+      "5",
+      "5"
+    ]
+  },
+  {
+    name: "Louie",
+    photo: "https://static.pexels.com/photos/58997/pexels-photo-58997.jpeg",
+    scores: [
+      "3",
+      "3",
+      "4",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    name: "Lou Ritter",
+    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
+    scores: [
+      "4",
+      "3",
+      "4",
+      "1",
+      "5",
+      "2",
+      "5",
+      "3",
+      "1",
+      "4"
+    ]
+  },
+  {
+    name: "Houdini",
+    photo: "https://static.pexels.com/photos/220974/pexels-photo-220974.jpeg",
+    scores: [
+      "4",
+      "4",
+      "2",
+      "3",
+      "2",
+      "2",
+      "3",
+      "2",
+      "4",
+      "5"
+    ]
+  }
+
+];
+
+module.exports = friends;
